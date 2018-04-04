1b8241ec45c73480d2445bb11b79ff8c1f114e79
Juan Antonio <ruchin10@gmail.com>
2018-04-05 01:25:32 +0200

    Added resolvers for lectures, teachers and classrooms

--- resolvers.js
+++ resolvers.js
@@ -4,73 +4,50 @@ import teacherModel from './models/teacher';
 import classroomModel from './models/classroom';
 
 const resolvers = {
+    Lecture: {
+        teacher(lecture) {
+            return teacherModel.findOne({id: lecture.teacher});
+        },
+        classroom(lecture) {
+            return classroomModel.findOne({id: lecture.classroom});
+        }
+    },
+    Student: {
+        lectures(student) {
+            return lectureModel.find({id: {$in: student.lectures}});
+        }
+    },
     Query: {
         students: () => {
-            return studentModel
-                .find({})
-                .populate({
-                    path:"lectures",
-                    populate: {path: "teacher classroom"}
-                });
+            return studentModel.find({});
         },
 
         student: (root, {id}) => {
-            return studentModel.findOne({id: id})
-                .populate({
-                    path:"lectures",
-                    populate: {path: "teacher classroom"}
-                });
+            return studentModel.findOne({id: id});
         },
 
         lectures: () => {
-            return lectureModel
-                .find({})
-                .populate({path: "teacher classroom"});
+            return lectureModel.find({});
         },
 
         lecture: (root, {id}) => {
-            return lectureModel
-                .findOne({id: id})
-                .populate({path: "teacher classroom"});
+            return lectureModel.findOne({id: id});
         },
 
         teachers: () => {
-            return teacherModel
-                .find({});
+            return teacherModel.find({});
         },
 
         teacher: (root, {id}) => {
-            return teacherModel
-                .findOne({id: id});
+            return teacherModel.findOne({id: id});
         },
 
         classrooms: () => {
-            return classroomModel
-                .find({});
+            return classroomModel.find({});
         },
 
         classroom: (root, {id}) => {
-            return classroomModel
-                .findOne({id: id});
-        }
-    },
-    Mutation: {
-        addStudent: (root, {name, idCard, birthdate, lectures}) => {
-            const student = new studentModel({
-                name: name,
-                birthdate: birthdate,
-                idCard: idCard
-            });
-
-            return student.save();
-        },
-
-        deleteStudent: (root, {id}) => {
-            return studentModel.findByIdAndRemove(id);
-        },
-        
-        updateStudent: (root, {id, name, idCard, birthdate, lectures}) => {
-
+            return classroomModel.findOne({id: id});
         }
     }
 }
